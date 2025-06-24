19d711bab94196d5ac5129c1ad68f6b4fe59f299
Muhammad Umer Khan <muhammadumerk546@gmail.com>
2025-06-24 11:41:15 +0500

    Update config.py

--- scripts/config.py
+++ scripts/config.py
@@ -8,7 +8,7 @@ PROCESSED_DATA_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..
 
 # Preprocessing parameters
 PREPROCESSING_CONFIG = {
-    'numerical_columns': ['loan_amnt', 'loan_int_rate', 'person_income', 'person_age', 'person_emp_length'],
+    'numerical_columns': ['person_age', 'person_income', 'person_emp_length', 'loan_amnt', 'loan_int_rate', 'loan_percent_income', 'cb_person_cred_hist_length'],
     'categorical_columns': ['loan_grade', 'cb_person_default_on_file', 'person_home_ownership', 'loan_intent'],
     'target_column': 'loan_status'
 }
